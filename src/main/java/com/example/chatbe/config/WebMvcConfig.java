a3d500813b08fd6ce7b474cd0bfba96be4ed6356
phatkill <choicodeforce@gmail.com>
2025-06-25 08:50:06 +0700

    add render deployment config and update to use Neon DB

--- src/main/java/com/example/chatbe/config/WebMvcConfig.java
+++ src/main/java/com/example/chatbe/config/WebMvcConfig.java
@@ -1,14 +1,14 @@
-package com.example.chatbe.config;
+            package com.example.chatbe.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+            import org.springframework.context.annotation.Configuration;
+            import org.springframework.web.servlet.config.annotation.*;
 
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+            @Configuration
+            public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/files/**")
-                .addResourceLocations("file:uploads/");
-    }
-}
+                @Override
+                public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                    registry.addResourceHandler("/files/**")
+                            .addResourceLocations("file:uploads/");
+                }
+            }
